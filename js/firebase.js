936030d7c87c8deecb698bdf565be3ce82f2d972
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2026-01-01 01:07:12 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,54 +1,12 @@
-// public/js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import {
-  getFirestore,
-  enableIndexedDbPersistence,
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  where,
-  serverTimestamp
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-/* ===============================
-   Firebase Config
-================================ */
 const firebaseConfig = {
   apiKey: "AIzaSyBZwWxWIIE0exAPoL9P8pbmp19gnBFxQq0",
   authDomain: "pos-pro-996f0.firebaseapp.com",
   projectId: "pos-pro-996f0",
-  storageBucket: "pos-pro-996f0.firebasestorage.app",
-  messagingSenderId: "591451935128",
-  appId: "1:591451935128:web:683495139e62fb9b1e1bed"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
 
-/* ===============================
-   Offline Persistence
-================================ */
-enableIndexedDbPersistence(db).catch(() => {
-  console.warn("Offline persistence already enabled or blocked");
-});
+const auth = firebase.auth();
+const db = firebase.firestore();
 
-/* ===============================
-   Helpers
-================================ */
-export function userCollection(uid, name) {
-  return collection(db, "users", uid, name);
-}
-
-export {
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  where,
-  serverTimestamp
-};
+db.enablePersistence().catch(()=>{});
